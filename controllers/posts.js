c0b9bf304a5af827c87caa2939a5c8f29298db5f
Denis <denis.r2211@gmail.com>
2023-05-29 23:43:58 +0300

    Added updated post

--- controllers/posts.js
+++ controllers/posts.js
@@ -49,10 +49,93 @@ export const createPost = async (req, res) => {
         return res.json({
             newPostWithoutImage,
             message: 'Пост успешно добавлен.'
-            })
+        })
 
     } catch (error) {
         console.error(error)
         res.json({ message: 'Что-то пошло не так!' })
+    }
+}
+
+// Get All Posts
+export const getAll = async (req, res) => {
+    try {
+        const posts = await Post.find().sort('-createAt')
+        const popularPosts = await Post.find().limit(5).sort('-views')
+
+        if (!posts) {
+            return res.json({ message: 'Постов нет.' })
+        }
+
+        res.json({ posts, popularPosts })
+    } catch (error) {
+        res.json({ message: 'Что-то пошло не так.' })
+    }
+}
+
+// Get Post By Id
+export const getById = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, {
+            $inc: { views: 1 },
+        })
+        res.json(post)
+    } catch (error) {
+        res.json({ message: 'Что-то пошло не так.' })
+    }
+}
+
+// Get My Posts
+export const getMyPosts = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+        const list = await Promise.all(
+            user.posts.map((post) => {
+                return Post.findById(post._id)
+            })
+        )
+        res.json(list)
+    } catch (error) {
+        res.json({ message: 'Что-то пошло не так.' })
+    }
+}
+
+// Remove post
+export const removePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id)
+        if (!post) return res.json({ message: 'Такого поста не существует.' })
+
+        await User.findByIdAndUpdate(req.userId, {
+            $pull: { posts: req.params.id }
+        })
+
+        res.json({ message: 'Пост удален.' })
+    } catch (error) {
+        res.json({ message: 'Что-то пошло не так.' })
+    }
+}
+
+// Update post
+export const updatePost = async (req, res) => {
+    try {
+        const { title, text, id } = req.body
+        const post = await Post.findById(id)
+
+        if (req.files) {
+            let fileName = Date.now().toString() + req.files.image.name
+            const __dirname = dirname(fileURLToPath(import.meta.url))
+            req.files.image.mv(path.join(__dirname, '..', 'uploads', fileName))
+            post.imgUrl = fileName || ''
+        }
+
+        post.title = title
+        post.text = text
+
+        await post.save()
+
+        res.json(post)
+    } catch (error) {
+        res.json({ message: 'Что-то пошло не так.' })
     }
 }
